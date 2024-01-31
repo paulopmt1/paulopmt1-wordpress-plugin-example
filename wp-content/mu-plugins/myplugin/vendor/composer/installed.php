02d6abccb05961cbd046d85924f94dd5b0ddb002
Paulo Trentin <paulo@paulotrentin.com.br>
2024-01-31 13:54:11 -0300

    Creating an initial test structure

---
+++ wp-content/mu-plugins/myplugin/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8f434f48a44473d702b3e0ab4583f72601a71cb6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8f434f48a44473d702b3e0ab4583f72601a71cb6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
